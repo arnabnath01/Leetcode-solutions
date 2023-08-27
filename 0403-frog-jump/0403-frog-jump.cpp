5189fd6ae532ca1c0c516738eda0c00c218b94e3
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-08-28 01:15:28 +0530

    Time: 152 ms (59.32%), Space: 51.7 MB (44.59%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,46 +1,18 @@
 class Solution {
 public:
-    unordered_map<int, int> m;
-    int dp[2000][2000];
-
-    int solve(int i, int k, vector<int>& stones) {
-        if (i == stones.size() - 1) {
-            return true;
-        }
-        
-        if (dp[i][k] != -1) {
-            return dp[i][k];
-        }
-
-        bool k0 = false;
-        bool kp = false;
-        bool k1 = false;
-
-        if (m.find(stones[i] + k) != m.end()) {
-            k0 = solve(m[stones[i] + k], k, stones);
-        }
-        if (k > 1 && m.find(stones[i] + k - 1) != m.end()) {
-            kp = solve(m[stones[i] + k - 1], k - 1, stones);
-        }
-        if (m.find(stones[i] + k + 1) != m.end()) {
-            k1 = solve(m[stones[i] + k + 1], k + 1, stones);
-        }
-
-        dp[i][k] = k0 || kp || k1;
-        return dp[i][k];
-    }
-
     bool canCross(vector<int>& stones) {
-        if (stones[1] - stones[0] != 1) {
-            return false;
+        unordered_map<int,unordered_set<int>> dp;
+        dp[1]={1};
+
+        for(int i=1;i<stones.size();++i){
+            for(auto jump: dp[stones[i]])
+            {
+                dp[stones[i]+jump].insert(jump);
+                dp[stones[i]+jump-1].insert(jump-1);
+                dp[stones[i]+jump+1].insert(jump+1);
+            }
         }
 
-        for (int i = 0; i < stones.size(); i++) {
-            m[stones[i]] = i;
-        }
-        
-        memset(dp, -1, sizeof(dp));
-
-        return solve(1, 1, stones);
+        return dp[stones.back()].size() != 0;
     }
 };
