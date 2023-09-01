50ef04a13e1584351b89cd3334c20c1e030e7612
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-09-01 15:25:52 +0530

    Time: 5 ms (73.32%), Space: 13.6 MB (34.47%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,13 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if (nums.empty()) {
-            return {-1, -1};
-        }
-        
-        int fst = -1, lst = -1;
-        int s = 0, e = nums.size() - 1;
-        
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (nums[m] == target) {
-                fst = m;
-                e = m - 1;
-            } else if (nums[m] > target) {
-                e = m - 1;
-            } else {
-                s = m + 1;
-            }
-        }
-        
-        s = 0, e = nums.size() - 1;
-        
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (nums[m] == target) {
-                lst = m;
-                s = m + 1;
-            } else if (nums[m] > target) {
-                e = m - 1;
-            } else {
-                s = m + 1;
-            }
-        }
-        
-        if (fst == -1 || lst == -1) {
-            return {-1, -1};
-        } else {
-            return {fst, lst};
-        }
+        if(nums.empty()) return {-1,-1};
+    if(!binary_search(nums.begin(),nums.end(),target)) return {-1,-1};
+
+
+        int left = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int right = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+    cout<<left <<" "<<right;
+return {left,right-1};
     }
 };
