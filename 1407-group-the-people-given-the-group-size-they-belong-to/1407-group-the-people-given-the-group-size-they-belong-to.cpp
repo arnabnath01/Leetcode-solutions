1a67e72b84dafee24cb2ab2dbab6aa6e60988e1a
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-09-11 19:18:40 +0530

    Time: 8 ms (78.38%), Space: 14 MB (12.32%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        // Initialize the answer vector to store groups of people
-        vector<vector<int>> groupsOfPeople;
-
-        // Create a map to store people's IDs grouped by their desired group sizes
-        map<int, vector<int>> groupMap;
-
-        // Iterate through the groupSizes array
-        for (int personID = 0; personID < groupSizes.size(); personID++) {
-            int desiredGroupSize = groupSizes[personID];
-
-            // Add the current person's ID to the corresponding group
-            groupMap[desiredGroupSize].push_back(personID);
-
-            // Check if the group is complete (i.e., has the desired size)
-            if (groupMap[desiredGroupSize].size() == desiredGroupSize) {
-                // Add the complete group to the answer vector
-                groupsOfPeople.emplace_back(move(groupMap[desiredGroupSize]));
-
-                // Clear the group as it's been added to the answer
-                groupMap[desiredGroupSize].clear();
-            }
+    vector<vector<int>> groupThePeople(vector<int>& groupsizes) {
+        unordered_map<int,vector<int>>u;
+        for(int i=0;i<groupsizes.size();i++){
+            u[groupsizes[i]].push_back(i);
         }
-
-        // Return the answer containing grouped people
-        return groupsOfPeople;
+        vector<vector<int>>v;
+        for(auto x:u){
+            int k=x.first;
+           vector<int>f=x.second;
+           int c=f.size()/k;
+           int l=0;
+           for(int i=0;i<c;i++){
+                vector<int>ans;
+               for(int j=0;j<k;j++){
+                ans.push_back(f[l]);
+                l++;
+               }
+               v.push_back(ans);
+           }
+        }
+        return v;
     }
 };
