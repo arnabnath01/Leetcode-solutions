3a94266e94dd7e668dfde81337d2ed7f73d7a502
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-09-23 14:46:19 +0530

    Time: 0 ms (100%), Space: 6.9 MB (11.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+int f(int i,int j, vector<vector<int>>&dp)
+{
+    // base cases
+    if(i==0 && j==0) return 1;
+    if(i<0 || j<0) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+
+    //doing stuffs
+    int up=f(i-1,j,dp);
+    int left = f(i,j-1,dp);
+
+    return dp[i][j]=up+left;
+}
     int uniquePaths(int m, int n) {
-     double ans = 1;
-     int N= m+n-2;
-     int r = m-1;
-        for(int i=1;i<=r;i++)
-        {
-            ans=ans*(N-r+i)/i;
-        }
-        return (int)ans;
+     vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,dp);
     }
 };
