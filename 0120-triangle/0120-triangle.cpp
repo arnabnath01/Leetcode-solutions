28dd53e1c8fec7fc72385132bd3c399185fc6970
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-09-30 22:21:01 +0530

    Time: 8 ms (28.99%), Space: 9.2 MB (14.67%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,11 +2,11 @@
 class Solution {
 public:
 
-    // Tabulation ( opposite flow of memoization)
+    // Space optimised solution
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
     vector<int>prev(n,0);
-        // filling the ground row   
+         
         for(int j=0;j<n;j++){
             prev[j]=triangle[n-1][j];
         }
@@ -16,8 +16,6 @@ public:
             vector<int>curr(n,0);
             for(int j=i;j>=0;j--)
             {
-                // if(i==n-1) return triangle[i][j];
-
                 int down= triangle[i][j]+prev[j];
                 int dr= triangle[i][j]+prev[j+1]; 
                curr[j]= min(down,dr);
