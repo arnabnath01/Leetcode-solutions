310372a98d2c42f2b68d63c9abcb9216f74d485e
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-09-30 20:20:32 +0530

    Time: 0 ms (100%), Space: 9.4 MB (5.6%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,17 +3,29 @@
 class Solution {
 public:
 
-    int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp,int n)
-    {
-        if(i==n-1) return triangle[i][j]; // whenever it reaches bottom, loop ends
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down= triangle[i][j]+f(i+1,j,triangle,dp,n);
-        int dr= triangle[i][j]+f(i+1,j+1,triangle,dp,n);
-        return dp[i][j]= min(down,dr);
-    }
+    // Tabulation ( opposite flow of memoization)
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(0,0,triangle,dp,n);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+
+        // filling the ground row
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+    
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                // if(i==n-1) return triangle[i][j];
+
+                int down= triangle[i][j]+dp[i+1][j];
+                int dr= triangle[i][j]+dp[i+1][j+1]; 
+                dp[i][j]= min(down,dr);
+            }
+        }
+
+
+        return dp[0][0];
     }
 };
