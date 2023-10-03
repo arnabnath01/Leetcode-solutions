909f33eba48bddcdc53f8f80a81e6fba10643561
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-10-04 01:02:57 +0530

    Time: 490 ms (10.74%), Space: 10.2 MB (89.24%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-// TABULATION
+// SPACE OPTIMIZATION
 
     bool canPartition(vector<int>& nums) {
         
@@ -10,22 +10,23 @@ public:
         int n = nums.size();
         //sum is even
         
-        vector<vector<bool>> dp(n,vector<bool>(sum/2+1,0));
+        vector<bool> prev(sum/2+1,0),curr(sum/2+1,0);
+        prev[0]=true,curr[0]=true;
         //base case 
-        for(int i =0;i<n;i++) dp[i][0]=true;
+        for(int i =0;i<n;i++) curr[0]=true;
         if(nums[0]<=sum/2)
-        dp[0][nums[0]]=true;
+        prev[nums[0]]=true;
 
         for(int ind=1;ind<n;ind++){
             for(int target=1;target<=sum/2;target++){
-                int nonTake=dp[ind-1][target];
+                int nonTake=prev[target];
                 int take=false;
-                if(nums[ind]<=target) take=dp[ind-1][target-nums[ind]];
-                dp[ind][target]=take || nonTake;
+                if(nums[ind]<=target) take=prev[target-nums[ind]];
+                curr[target]=take || nonTake;
             }
+            prev=curr;
         }
-        return dp[n-1][sum/2];
-
+        return prev[sum/2];
         // return subsetSumToK(n,sum/2,nums);
     }
 };
