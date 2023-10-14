74685bedd3a41c0e242b1bc7a31253d8349c08b3
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2023-10-14 23:19:04 +0530

    Time: 101 ms (90.17%), Space: 89.5 MB (92.51%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -3,9 +3,9 @@
 
 class Solution {
 public:
-    int paintWalls(std::vector<int>& cost, std::vector<int>& time) {
+    int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size(); // Get the number of walls
-        std::vector<int> dp(n + 1, 1e9); // Create a dynamic programming array to store the minimum cost
+        vector<int> dp(n + 1, 1e9); // Create a dynamic programming array to store the minimum cost
         dp[0] = 0; // The minimum cost to paint 0 walls is 0 (base case)
 
         // Loop through each wall
@@ -16,7 +16,7 @@ public:
                 // The minimum cost at 'j' is the minimum of:
                 //   - The cost of painting the current wall 'i' + the cost of painting the previous walls (dp[j - time[i] - 1])
                 //   - The cost of painting the previous walls without using the current wall 'i' (dp[j])
-                dp[j] = std::min(dp[j], dp[std::max(j - time[i] - 1, 0)] + cost[i]);
+                dp[j] = min(dp[j], dp[max(j - time[i] - 1, 0)] + cost[i]);
             }
         }
         return dp[n]; // The minimum cost to paint all 'n' walls is stored in dp[n]
