b77bfe94599517d8087a20e77dab931054ff511a
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-06-24 21:32:49 +0530

    Time: 103 ms (5.33%), Space: 72.7 MB (5.97%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,61 +1,45 @@
-
-
-//O(N2) solution
-
-// class Solution {
-// public:
-//     bool ifNeg(vector<int> nums)
-//     {
-//         for(auto i:nums)
-//         {
-//             if(i>=0 ) return false;
-//         }
-//         return true;
-//     }
-//     bool ifPos(vector<int> nums)
-//     {
-//         for(auto i:nums)
-//         {
-//             if(i<0 ) return false;
-//         }
-//         return true;
-//     }
-//     int maxSubArray(vector<int>& nums) {
-       
-//        if(ifNeg(nums))
-//        return *max_element(nums.begin(), nums.end());
-
-//        if(ifPos(nums))
-//        {
-//            int sum=0;
-//            for(auto i:nums)
-//            sum+=i;
-//            return sum;
-//        }
-//     int maxSum=0,curSum=0;
-//     for(int i=0;i<nums.size();i++)
-//     {
-        
-
-//         curSum+=nums[i];
-//         if(curSum>maxSum)
-//         maxSum=curSum;
-//         if(curSum<0)
-//         curSum=0;
-
-//     }
-//     return maxSum;
-//     }
-// };
-
-
 class Solution {
 public:
+    bool ifNeg(vector<int> nums)
+    {
+        for(auto i:nums)
+        {
+            if(i>=0 ) return false;
+        }
+        return true;
+    }
+    bool ifPos(vector<int> nums)
+    {
+        for(auto i:nums)
+        {
+            if(i<0 ) return false;
+        }
+        return true;
+    }
     int maxSubArray(vector<int>& nums) {
-        int curMax = 0, maxTillNow = INT_MIN;
-        for(auto c : nums)
-            curMax = max(c, curMax + c),
-            maxTillNow = max(maxTillNow, curMax);
-        return maxTillNow;
+       
+       if(ifNeg(nums))
+       return *max_element(nums.begin(), nums.end());
+
+       if(ifPos(nums))
+       {
+           int sum=0;
+           for(auto i:nums)
+           sum+=i;
+           return sum;
+       }
+    int maxSum=0,curSum=0;
+    for(int i=0;i<nums.size();i++)
+    {
+        
+
+        curSum+=nums[i];
+        if(curSum>maxSum)
+        maxSum=curSum;
+        if(curSum<0)
+        curSum=0;
+
+    }
+    return maxSum;
     }
 };
