3322b5ec50d1d10c91f60fa4ed7e9056e3913063
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-06-25 01:07:19 +0530

    Time: 98 ms (32.06%), Space: 113.2 MB (22.24%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+// O(n) time complexity 
+// O(n) space complexity 
     int minKBitFlips(vector<int>& nums, int k) {
         int n = nums.size();
         int times = 0, ans = 0;
@@ -7,11 +9,13 @@ public:
 
         for (int i = 0; i < n; i++) {
 
+            // setting the first limit
             if (i >= k)
                 times -= flipped[i - k];
 
             if ((times % 2 == 0 && nums[i] == 0) ||
                 (times % 2 == 1 && nums[i] == 1)) {
+                // setting the last limit
                 if (i + k > n)
                     return -1;
                 times++;
