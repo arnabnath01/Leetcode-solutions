5e1ca70402f60d0ccca599ee69ed281045277df5
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-08-25 22:19:40 +0530

    Time: 0 ms (100%), Space: 9.9 MB (74.76%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,34 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// using 1 stack
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>postorder;
-        stack<TreeNode*>st;
-        TreeNode* curr=root;
-        while(curr!=nullptr || !st.empty())
-        {
-            if(curr!=nullptr)
-            {
-                st.push(curr);
-                curr=curr->left;
-            }
-            else{
-                auto temp=st.top()->right;
-              
-                if(temp==nullptr){
-                    temp=st.top();
-                    st.pop();
+        vector<int> postorder;
+        if (root == nullptr) return postorder;
+        
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        TreeNode* lastVisited = nullptr;
 
-                    postorder.push_back(temp->val);
-                    while(!st.empty() && st.top()->right!=nullptr){
-                        temp=st.top();
-                        st.pop();
-                        postorder.push_back(temp->val);
-                    }
+        while (curr != nullptr || !st.empty()) {
+            if (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                TreeNode* peekNode = st.top();
+                if (peekNode->right != nullptr && lastVisited != peekNode->right) {
+                    curr = peekNode->right;
+                } else {
+                    postorder.push_back(peekNode->val);
+                    lastVisited = st.top();
+                    st.pop();
                 }
-                else curr=temp;
             }
         }
 
