ddb6542b630809322b8f2bed5d033b20727868c6
ARNAB NATH <98618274+arnabnath01@users.noreply.github.com>
2024-09-25 12:56:33 +0530

    Time: 68 ms (89.64%), Space: 67.8 MB (99.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,45 +1,61 @@
-class Solution {
-public:
-    bool ifNeg(vector<int> nums)
-    {
-        for(auto i:nums)
-        {
-            if(i>=0 ) return false;
-        }
-        return true;
-    }
-    bool ifPos(vector<int> nums)
-    {
-        for(auto i:nums)
-        {
-            if(i<0 ) return false;
-        }
-        return true;
-    }
-    int maxSubArray(vector<int>& nums) {
-       
-       if(ifNeg(nums))
-       return *max_element(nums.begin(), nums.end());
 
-       if(ifPos(nums))
-       {
-           int sum=0;
-           for(auto i:nums)
-           sum+=i;
-           return sum;
-       }
-    int maxSum=0,curSum=0;
-    for(int i=0;i<nums.size();i++)
-    {
+
+//O(N2) solution
+
+// class Solution {
+// public:
+//     bool ifNeg(vector<int> nums)
+//     {
+//         for(auto i:nums)
+//         {
+//             if(i>=0 ) return false;
+//         }
+//         return true;
+//     }
+//     bool ifPos(vector<int> nums)
+//     {
+//         for(auto i:nums)
+//         {
+//             if(i<0 ) return false;
+//         }
+//         return true;
+//     }
+//     int maxSubArray(vector<int>& nums) {
+       
+//        if(ifNeg(nums))
+//        return *max_element(nums.begin(), nums.end());
+
+//        if(ifPos(nums))
+//        {
+//            int sum=0;
+//            for(auto i:nums)
+//            sum+=i;
+//            return sum;
+//        }
+//     int maxSum=0,curSum=0;
+//     for(int i=0;i<nums.size();i++)
+//     {
         
 
-        curSum+=nums[i];
-        if(curSum>maxSum)
-        maxSum=curSum;
-        if(curSum<0)
-        curSum=0;
+//         curSum+=nums[i];
+//         if(curSum>maxSum)
+//         maxSum=curSum;
+//         if(curSum<0)
+//         curSum=0;
 
-    }
-    return maxSum;
+//     }
+//     return maxSum;
+//     }
+// };
+
+//KADANE'S ALGORITHM
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curMax = 0, maxTillNow = INT_MIN;
+        for(auto c : nums)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
     }
 };
